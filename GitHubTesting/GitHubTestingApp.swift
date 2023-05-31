e1bba9c90d28e7b5ce93c7cdc19001487969369b
Isabel <isabel.lim09@rafflesgirlssch.edu.sg>
2023-05-31 11:08:23 +0800

    Initial Commit

---
+++ GitHubTesting/GitHubTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestingApp.swift
+//  GitHubTesting
+//
+//  Created by Isabel Lim on 31/5/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
